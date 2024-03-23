c78373f7fdc50fc8000505ea74712d216ce5bfe2
christyl23 <120269677+christyl23@users.noreply.github.com>
2024-03-22 23:52:25 -0400

    updates

--- nginx/ssl-encrypt-myjenkins.sh
+++ nginx/ssl-encrypt-myjenkins.sh
@@ -7,7 +7,7 @@ CONFIG_FILE="/etc/nginx/sites-available/$DOMAIN"
 # Create Nginx configuration file
 sudo tee "$CONFIG_FILE" > /dev/null <<'EOL'
 upstream jenkins{
-    server 107.21.79.176:8080;
+    server 127.0.0.1:8080;
 }
 
 server{
