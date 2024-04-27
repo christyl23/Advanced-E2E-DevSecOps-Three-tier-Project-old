3e3780014a0a86baf0701b81e2918520dd7bfcd0
christyl23 <120269677+christyl23@users.noreply.github.com>
2024-04-26 23:17:35 -0400

    Update ec2.tf

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami                    = var.ami
-  instance_type          = "t2.large"
+  instance_type          = "t2.xlarge"
   key_name               = var.key-name
   subnet_id              = aws_subnet.public-subnet.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
